35b2aa5d219a2db67a21fc3c69e5b4222c8df83c
John Daiker <daikerjohn@gmail.com>
2020-10-10 13:40:33 -0700

    Attempt fix at RDP scaling

--- mRemoteNG/Connection/Protocol/RDP/RdpProtocol6.cs
+++ mRemoteNG/Connection/Protocol/RDP/RdpProtocol6.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Timers;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using AxMSTSCLib;
 using mRemoteNG.App;
 using mRemoteNG.Messages;
@@ -514,13 +519,249 @@ namespace mRemoteNG.Connection.Protocol.RDP
             }
         }
 
+        internal static class DpiManager
+        {
+            private static int m_scalePercent = 0;
+            private const float c_StandardDpi = 96f; // Used for GetDeviceCaps().
+
+            public static int GetScalePercent()
+            {
+                if (m_scalePercent == 0)
+                {
+                    Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+                    IntPtr desktop = g.GetHdc();
+
+                    int Xdpi = GetDeviceCaps(desktop, DeviceCap.LOGPIXELSX);
+                    /*
+                    foreach (DeviceCap cap in Enum.GetValues(typeof(DeviceCap)))
+                    {
+                        int result = GetDeviceCaps(hdc, cap);
+                        Console.WriteLine(string.Format("{0}: {1}", cap, result));
+                    }
+                    */
+
+                    g.ReleaseHdc();
+                    m_scalePercent = (int)Math.Round((Xdpi / c_StandardDpi) * 100);
+                }
+                return m_scalePercent;
+            }
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr GetDC(IntPtr hWnd);
+
+            [DllImport("gdi32.dll")]
+            private static extern int GetDeviceCaps(IntPtr hdc, DeviceCap nIndex);
+            public enum DeviceCap
+            {
+                /// <summary>
+                /// Device driver version
+                /// </summary>
+                DRIVERVERSION = 0,
+                /// <summary>
+                /// Device classification
+                /// </summary>
+                TECHNOLOGY = 2,
+                /// <summary>
+                /// Horizontal size in millimeters
+                /// </summary>
+                HORZSIZE = 4,
+                /// <summary>
+                /// Vertical size in millimeters
+                /// </summary>
+                VERTSIZE = 6,
+                /// <summary>
+                /// Horizontal width in pixels
+                /// </summary>
+                HORZRES = 8,
+                /// <summary>
+                /// Vertical height in pixels
+                /// </summary>
+                VERTRES = 10,
+                /// <summary>
+                /// Number of bits per pixel
+                /// </summary>
+                BITSPIXEL = 12,
+                /// <summary>
+                /// Number of planes
+                /// </summary>
+                PLANES = 14,
+                /// <summary>
+                /// Number of brushes the device has
+                /// </summary>
+                NUMBRUSHES = 16,
+                /// <summary>
+                /// Number of pens the device has
+                /// </summary>
+                NUMPENS = 18,
+                /// <summary>
+                /// Number of markers the device has
+                /// </summary>
+                NUMMARKERS = 20,
+                /// <summary>
+                /// Number of fonts the device has
+                /// </summary>
+                NUMFONTS = 22,
+                /// <summary>
+                /// Number of colors the device supports
+                /// </summary>
+                NUMCOLORS = 24,
+                /// <summary>
+                /// Size required for device descriptor
+                /// </summary>
+                PDEVICESIZE = 26,
+                /// <summary>
+                /// Curve capabilities
+                /// </summary>
+                CURVECAPS = 28,
+                /// <summary>
+                /// Line capabilities
+                /// </summary>
+                LINECAPS = 30,
+                /// <summary>
+                /// Polygonal capabilities
+                /// </summary>
+                POLYGONALCAPS = 32,
+                /// <summary>
+                /// Text capabilities
+                /// </summary>
+                TEXTCAPS = 34,
+                /// <summary>
+                /// Clipping capabilities
+                /// </summary>
+                CLIPCAPS = 36,
+                /// <summary>
+                /// Bitblt capabilities
+                /// </summary>
+                RASTERCAPS = 38,
+                /// <summary>
+                /// Length of the X leg
+                /// </summary>
+                ASPECTX = 40,
+                /// <summary>
+                /// Length of the Y leg
+                /// </summary>
+                ASPECTY = 42,
+                /// <summary>
+                /// Length of the hypotenuse
+                /// </summary>
+                ASPECTXY = 44,
+                /// <summary>
+                /// Shading and Blending caps
+                /// </summary>
+                SHADEBLENDCAPS = 45,
+
+                /// <summary>
+                /// Logical pixels inch in X
+                /// </summary>
+                LOGPIXELSX = 88,
+                /// <summary>
+                /// Logical pixels inch in Y
+                /// </summary>
+                LOGPIXELSY = 90,
+
+                /// <summary>
+                /// Number of entries in physical palette
+                /// </summary>
+                SIZEPALETTE = 104,
+                /// <summary>
+                /// Number of reserved entries in palette
+                /// </summary>
+                NUMRESERVED = 106,
+                /// <summary>
+                /// Actual color resolution
+                /// </summary>
+                COLORRES = 108,
+
+                // Printing related DeviceCaps. These replace the appropriate Escapes
+                /// <summary>
+                /// Physical Width in device units
+                /// </summary>
+                PHYSICALWIDTH = 110,
+                /// <summary>
+                /// Physical Height in device units
+                /// </summary>
+                PHYSICALHEIGHT = 111,
+                /// <summary>
+                /// Physical Printable Area x margin
+                /// </summary>
+                PHYSICALOFFSETX = 112,
+                /// <summary>
+                /// Physical Printable Area y margin
+                /// </summary>
+                PHYSICALOFFSETY = 113,
+                /// <summary>
+                /// Scaling factor x
+                /// </summary>
+                SCALINGFACTORX = 114,
+                /// <summary>
+                /// Scaling factor y
+                /// </summary>
+                SCALINGFACTORY = 115,
+
+                /// <summary>
+                /// Current vertical refresh rate of the display device (for displays only) in Hz
+                /// </summary>
+                VREFRESH = 116,
+                /// <summary>
+                /// Horizontal width of entire desktop in pixels
+                /// </summary>
+                DESKTOPVERTRES = 117,
+                /// <summary>
+                /// Vertical height of entire desktop in pixels
+                /// </summary>
+                DESKTOPHORZRES = 118,
+                /// <summary>
+                /// Preferred blt alignment
+                /// </summary>
+                BLTALIGNMENT = 119
+            }
+
+            [DllImport("user32.dll")]
+            private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDc);
+        }
+
+        protected uint GetDesktopScaleFactor()
+        {
+            switch (DpiManager.GetScalePercent())
+            {
+                case 125:
+                    return 125;
+                case 150:
+                case 175:
+                    return 150;
+                case 200:
+                    return 200;
+            }
+            if (DpiManager.GetScalePercent() > 200)
+            {
+                return 200;
+            }
+            return 100;
+        }
+        protected uint GetDeviceScaleFactor()
+        {
+            switch (DpiManager.GetScalePercent())
+            {
+                case 125:
+                case 150:
+                case 175:
+                    return 140;
+                case 200:
+                    return 180;
+            }
+            if (DpiManager.GetScalePercent() > 200)
+            {
+                return 180;
+            }
+            return 100;
+        }
+
         private void SetResolution()
         {
             try
             {
-                var scaleFactor = (uint)_displayProperties.ResolutionScalingFactor.Width * 100;
-                SetExtendedProperty("DesktopScaleFactor", scaleFactor);
-                SetExtendedProperty("DeviceScaleFactor", (uint)100);
+                SetExtendedProperty("DesktopScaleFactor", this.GetDesktopScaleFactor());
+                SetExtendedProperty("DeviceScaleFactor", this.GetDeviceScaleFactor());
 
                 if (Force.HasFlag(ConnectionInfo.Force.Fullscreen))
                 {
